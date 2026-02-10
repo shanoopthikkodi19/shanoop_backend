75a4c65ffa84276437ab5608edc528489578679f
SHANOOP T K <shanoopthikkodi19@gmail.com>
2026-02-10 15:20:09 +0530

    user registration added

---
+++ shanoop/shanoop_backend/routes/authentication.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+var router = express.Router();
+
+const {userRegister} = require("../services/authentication.service");
+
+router.post("/register", async (req, res, next) => {
+  let body = req.body;
+  let response = await userRegister(body);
+  res.json(response);
+});
+
+module.exports = router;
+
